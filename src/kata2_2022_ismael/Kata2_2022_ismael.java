dda3f6b5a6b256144623d94749a02202ce5a9f63
ismar <ismar@LAPTOP-B5B935IL>
2022-09-30 18:26:56 +0100

    Segunda versión kata2

--- src/kata2_2022_ismael/Kata2_2022_ismael.java
+++ src/kata2_2022_ismael/Kata2_2022_ismael.java
@@ -1,5 +1,7 @@
 package kata2_2022_ismael;
 
+import static java.rmi.Naming.list;
+import static java.util.Collections.list;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -8,21 +10,14 @@ import static jdk.nashorn.tools.ShellFunctions.input;
 public class Kata2_2022_ismael {
 
     public static void main(String[] args) {
-        int [] data= {4, 2, 6, 4, 9, 8, 0, 1, 2, 6, 3, 7, 8, 5};
+        Integer [] data= {4, 2, 6, 4, 9, 8, 0, 1, 2, 6, 3, 7, 8, 5};
         Map<Integer, Integer>histogram = new HashMap<Integer, Integer>();
        
         
-       for (int i = 0; i < data.length; i++) {
-           if (histogram.containsKey(data[i])){
-               histogram.put(data[i], histogram.get(data[i]) + 1);
-           }
-           else{
-               histogram.put(data[i], 1);
-           }
-               
+       for (int i = 0; i < data.length; i++) { 
+           histogram.put(data[i], histogram.containsKey(data[i])? histogram.get(data[i]) + 1 : 1);
        }
-        
-        for ( Map.Entry<Integer, Integer> entry: histogram.entrySet()){
+        for( Map.Entry<Integer, Integer> entry: histogram.entrySet()){
             System.out.println( entry.getKey() + "==>" + entry.getValue());
         }
         
