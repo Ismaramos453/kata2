36656665ab73a7a2e6d7a9f9f685ec3f30b7fd36
ismar <ismar@LAPTOP-B5B935IL>
2022-10-05 16:09:16 +0100

    Quinta versión kata2

--- src/kata2_2022_ismael/Kata2_2022_ismael.java
+++ src/kata2_2022_ismael/Kata2_2022_ismael.java
@@ -7,12 +7,13 @@ public class Kata2_2022_ismael {
     
     public static void main(String[] args) {
         //int [] dato = {1,6,2,8,3,8,2,7,3,4,2,9,0,6,0};
-        Integer [] dato = {1,6,2,8,3,8,2,7,3,4,2,9,0,6,0};
+        //Integer [] dato = {1,6,2,8,3,8,2,7,3,4,2,9,0,6,0};
+        String[] dato = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         
         HistogramGenerator histo = new HistogramGenerator(dato);
 
-        Map<Integer, Integer> histogr = histo.getHistogram();
-        for(Map.Entry<Integer, Integer> entry: histogr.entrySet()){
+        Map<String, Integer> histogr = histo.getHistogram();
+        for(Map.Entry<String, Integer> entry: histogr.entrySet()){
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
         }
     }  
